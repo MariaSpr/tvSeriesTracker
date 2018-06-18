d7b3b2b52c50c1b7a38a6771d52a6c3e6d15d052
Maria Siapera <mariaspr@unipi.gr>
2018-06-18 18:00:30 +0300

    -Set up firebase initializeApp

--- App.js
+++ App.js
@@ -6,11 +6,25 @@
 
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
+import firebase from 'firebase'
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 
 
 
+
 export default class App extends Component {
+    componentWillMount() {
+        // Firebase initialization before rendering
+        firebase.initializeApp({
+            apiKey: "AIzaSyCHxI4YPfCm0Db9D584rv_oPqRTJa_cAIk",
+            authDomain: "tvseriestracker-54011.firebaseapp.com",
+            databaseURL: "https://tvseriestracker-54011.firebaseio.com",
+            projectId: "tvseriestracker-54011",
+            storageBucket: "tvseriestracker-54011.appspot.com",
+            messagingSenderId: "419188042738"
+        })
+
+    }
   render() {
     return (
         <Container>
