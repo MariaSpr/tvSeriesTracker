9b7a0586f030bace2b1d921ae37f725070b6e2dd
Maria Siapera <mariaspr@unipi.gr>
2018-06-18 22:59:40 +0300

    -Added firebase auth, -Added error handling -Added spinner and user feedback

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
 import {StyleSheet, View} from 'react-native';
-import { Container, Header, Content, Footer, Text, Card, CardItem, Body, Button, Form, Item, Label, Input } from 'native-base';
+import { Container, Header, Content, Footer, Text, Card, CardItem, Body, Button, Form, Item, Label, Input, Spinner } from 'native-base';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        error: '',
+        loading: false
         };
+
+    onLoginButtonPressed() {
+        const {email, password}  = this.state;
+        this.setState({error:'', loading: true});
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch(()=>{
+                console.log(email);
+                console.log(password);
+                firebase.auth().createUserWithEmailAndPassword(email,password)
+                    .then(this.onLoginSuccess.bind(this))
+                    .catch(this.onLoginFail.bind(this));
+            });
+    }
+
+    onLoginSuccess() {
+        console.log("success");
+        this.setState({
+            email: '',
+            password: '',
+            loading: false,
+            error: ''
+        });
+    }
+
+    renderHelper() {
+        if(this.state.loading) {
+            return <Spinner color='blue' />;
+
+        }
+        return (
+            <Button block rounded primary onPress={this.onLoginButtonPressed.bind(this)}>
+                <Text>sign in</Text>
+            </Button>
+        );
+    }
+
+    onLoginFail() {
+        console.log("fail");
+        this.setState({ error: 'Authentication Failed', loading: false });
+        if(this.state.password.length < 6) {
+            this.setState({ error: 'Password must be longer than 6 characters', loading: false });
+        }
+    }
+
+
 
     render() {
         return (
@@ -35,12 +84,11 @@ class LoginForm extends Component {
                         </CardItem>
                         <CardItem >
                             <Body>
-                                <Button block rounded primary onPress={() => alert("This is Button")}>
-                                    <Text>sign in</Text>
-                                </Button>
+                            {this.renderHelper()}
                             </Body>
                         </CardItem>
                     </Card>
+                    <Text style={styles.errorText}>{this.state.error}</Text>
                 </Content>
                 <Footer />
             </Container>
@@ -53,6 +101,11 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    errorText: {
+        color: 'red',
+        alignSelf:'center',
+        fontSize: 20
     }
 });
 
