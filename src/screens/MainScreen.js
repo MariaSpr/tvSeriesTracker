ebd4208bb7b8972e91ce9d346b41468a7da24d98
Maria Siapera <mariaspr@unipi.gr>
2018-06-20 23:56:17 +0300

    -Fixed not working drawer button

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Spinner } from 'native-base';
-import LoginForm from '../components/LoginForm';
+import {DrawerActions} from "react-navigation";
 
 class MainScreen extends Component {
+
+
+    toggleDrawer = () => {
+        this.props.navigation.dispatch(DrawerActions.toggleDrawer())
+    }
+
     render() {
         const { navigate } = this.props.navigation;
         return (
@@ -10,7 +16,7 @@ class MainScreen extends Component {
                     <Left>
                         <Button
                             transparent
-                            onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+                            onPress={this.toggleDrawer}>
                             <Icon name="menu" />
                         </Button>
                     </Left>
