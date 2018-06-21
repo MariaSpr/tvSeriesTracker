d493d727ded453a8013fbe052cb094aed1154145
Maria Siapera <mariaspr@unipi.gr>
2018-06-21 14:26:09 +0300

    -Added boilerplate ShowList component

---
+++ src/components/common/ShowList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {StyleSheet, View} from 'react-native';
+import { Container, Header, Content, Footer, Text, Card, CardItem, Body, Button, Form, Item, Label, Input, Spinner } from 'native-base';
+import firebase from 'firebase';
+
+
+class ShowList extends Component{
+    render(){
+        return (
+            <Text>This is ShowList Component</Text>
+        );
+    }
+}
+
+export default ShowList;
--- src/screens/Tabs/ExploreTab.js
+++ src/screens/Tabs/ExploreTab.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Container, Header, Title, Tab, Tabs, Button, Left, Right, Body, Icon, Text, Spinner } from 'native-base';
+import ShowList from '../../components/common/ShowList';
 
 class ExploreTab extends Component {
 
@@ -8,7 +9,7 @@ class ExploreTab extends Component {
     render() {
         return (
             <View>
-                <Text> This is explore tab</Text>
+               <ShowList />
             </View>
 
 
