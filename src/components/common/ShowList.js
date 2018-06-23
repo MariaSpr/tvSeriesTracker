dc0d3e88063ebf1822f41c1934b186c7fb55e344
Maria Siapera <mariaspr@unipi.gr>
2018-06-23 21:05:44 +0300

    -Load more pages

--- src/components/common/ShowList.js
+++ src/components/common/ShowList.js
@@ -4,11 +4,11 @@ import { List, ListItem, Text, Thumbnail, Body, Left } from 'native-base';
 import axios from 'axios';
 import ShowDetail from './ShowDetail';
 import {TMDB_URL, TMDB_IMG_URL, TMDB_API_KEY} from './api';
-import firebase from 'firebase';
+import InfiniteScroll from 'react-native-infinite-scroll';
 
 
 class ShowList extends Component{
-    state = {shows: []};
+    state = {shows: [], page: 1};
 
     componentWillMount(){
         axios.get(TMDB_URL+'tv/popular'+TMDB_API_KEY)
@@ -17,7 +17,25 @@ class ShowList extends Component{
             })
             .catch(err => {console.log(err)})
 
+        //Load more pages
+        var pageCount = this.state.page;
+
+        if(pageCount <= 5) {
+            pageCount++;
+            console.log(pageCount);
+            this.setState({page: pageCount});
+
+            axios.get(TMDB_URL+'tv/popular'+TMDB_API_KEY+"&page="+pageCount)
+                .then(res => {this.setState({shows: this.state.shows.concat(res.data.results)});
+                    console.log(res.data.results);
+                    console.log(this.state.shows);
+                })
+                .catch(err => {console.log(err)})
+        }
+
     }
+
+
 
     renderShows() {
         return this.state.shows.map((show,index) =>
@@ -25,7 +43,12 @@ class ShowList extends Component{
         );
     }
 
+
+
     render(){
+
+
+
         return (
             <ScrollView>
                 {this.renderShows()}
